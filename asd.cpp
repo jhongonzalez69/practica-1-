0324e7ba248c63ce234688336bf1307d34ebf1e7
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:26:59 -0500

    guardamos primer cambio

---
+++ asd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<stdio.h>
+
+int main (){
+    int a , b , suma;
+    scanf("%i", &a);
+    scanf("%i", &b);
+    suma = a+b ;
+    printf ("la suma es = %i" , suma );
+
+   return 0;
+    }
+
